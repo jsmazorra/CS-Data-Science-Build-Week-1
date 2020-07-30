3749c9dd9c6a0354a2bb71f8e94444185ae29830
jsmazorra <60892622+jsmazorra@users.noreply.github.com>
2020-07-30 00:41:24 -0400

    Second test for performance comparison using the digits dataset.

---
+++ second_test_knn.py
@@ -0,0 +1,54 @@
+from knn import Knn
+from sklearn.datasets import load_digits
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+# For this test I will be using the digits dataset from sklearn.
+digits = load_digits()
+data = digits.data
+target = digits.target
+
+# In this test, I'll be comparing the performance of my knn algorithm with
+# sklearn's own K-Neighbors Classifier.
+
+X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=0.3)
+
+
+# Sklearn-learn KNN Classifier
+# Instantiate model
+clf = KNeighborsClassifier(n_neighbors=10)
+
+# Fit
+clf.fit(X_train, y_train)
+
+# Prediction
+predict = clf.predict(X_test)
+print("---SKLEARN---")
+print("Prediction", predict)
+
+# Accuracy Score
+print(f"Sklearn's KNN classifier accuracy: {accuracy_score(y_test, predict)}")
+
+# y_pred
+y_pred = clf.predict([X_test[0]])
+print("y_pred", y_pred)
+
+# k_nearest_neighbors (build model)
+# Instantiate model
+classifier = Knn(n_neighbors=10)
+
+# Fit
+classifier.fit(X_train, y_train)
+
+# Prediction
+predict = classifier.predictor(X_test)
+print("---BUILD MODEL---")
+print("Prediction", predict)
+
+# Accuracy Score
+print(f"Build's k_nearest_neighbors model accuracy: {accuracy_score(y_test, predict)}")
+
+# y_pred
+y_pred = classifier.predictor([X_test[0]])
+print("y_pred", y_pred)
