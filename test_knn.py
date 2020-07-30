80a587150e72dfb35a566a7256d82fb76cdaf19d
jsmazorra <60892622+jsmazorra@users.noreply.github.com>
2020-07-30 00:38:25 -0400

    Test for performance comparison using the wine quality dataset

--- test_knn.py
+++ test_knn.py
@@ -1,12 +1,54 @@
 from knn import Knn
-from sklearn.datasets import load_boston
+from sklearn.datasets import load_wine
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
-# Let's load the data, I will be using the dataset of house prices in Boston.
-boston = load_boston()
-data = boston.data
-target = iris.target
+# Let's load the data, I will be using the wine quality dataset directly from sklearn.
+wine = load_wine()
+data = wine.data
+target = wine.target
 
 # In this test, I'll be comparing the performance of my knn algorithm with
 # sklearn's own K-Neighbors Classifier.
+
+X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=0.3)
+
+
+# Sklearn-learn KNN Classifier
+# Instantiate model
+clf = KNeighborsClassifier(n_neighbors=10)
+
+# Fit
+clf.fit(X_train, y_train)
+
+# Prediction
+predict = clf.predict(X_test)
+print("---SKLEARN---")
+print("Prediction", predict)
+
+# Accuracy Score
+print(f"Sklearn's KNN classifier accuracy: {accuracy_score(y_test, predict)}")
+
+# y_pred
+y_pred = clf.predict([X_test[0]])
+print("y_pred", y_pred)
+
+# k_nearest_neighbors (build model)
+# Instantiate model
+classifier = Knn(n_neighbors=10)
+
+# Fit
+classifier.fit(X_train, y_train)
+
+# Prediction
+predict = classifier.predictor(X_test)
+print("---BUILD MODEL---")
+print("Prediction", predict)
+
+# Accuracy Score
+print(f"Build's k_nearest_neighbors model accuracy: {accuracy_score(y_test, predict)}")
+
+# y_pred
+y_pred = classifier.predictor([X_test[0]])
+print("y_pred", y_pred)
